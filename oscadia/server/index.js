a9fec73e678dc5c66796017ffdcd21f4b88c7958
cucumber555 <mr.cucumber5794@gmail.com>
2026-09-24 11:35:00 +0900

    Update index.js

--- oscadia/server/index.js
+++ oscadia/server/index.js
@@ -23,6 +23,12 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+console.log(
+    "DB CONFIG:",
+    process.env.DATABASE_URL
+        ? process.env.DATABASE_URL.replace(/:[^:@]+@/, ":****@")
+        : "DATABASE_URL 없음"
+);
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL
 });
